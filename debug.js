871260a64cb1aab4c34dd835f530c27135b5d16d
CoderPuppy <cpup@cpup-laptop>
2014-07-05 00:32:14 -0400

    Betterify the debug system

--- debug.js
+++ debug.js
@@ -1,1 +1,23 @@
-module.exports = require('debug')('movie-voting')
+const debug = require('debug')
+
+const debugFns = {}
+
+function getDebug() {
+	const nameParts = [].slice.call(arguments)
+	const name = nameParts.join(':')
+	if(debugFns[name]){
+		return debugFns[name]
+	} else {
+		const debugFn = debug(name)
+		debugFn.name = name
+		debugFn.nameParts = nameParts
+		if(nameParts.length > 1)
+			debugFn.parent = getDebug.apply(null, nameParts.slice(0, nameParts.length - 1))
+		debugFn.sub = function() {
+			return getDebug.apply(null, nameParts.concat([].slice.call(arguments)))
+		}
+		debugFns[name] = debugFn
+		return debugFn
+	}
+}
+module.exports = getDebug('movie-voting')
