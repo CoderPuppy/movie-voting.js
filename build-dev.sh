d7f788d49fb4dd126788bd565411307de77e4674
CoderPuppy <cpup@cpup-laptop>
2014-07-07 13:25:29 -0400

    Include the main js in the vote js

--- build-dev.sh
+++ build-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-watchify            client.js -d -o public/index.js &
-watchify pages/vote/client.js -d -o public/vote.js  &
+# watchify            client.js -d -o public/index.js &
+watchify client.js pages/vote/client.js -d -o public/vote.js  &
 stylus -w pages/**/*.styl public/**/*.styl &
