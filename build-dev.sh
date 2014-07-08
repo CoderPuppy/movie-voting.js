27cff8b80cc9df45cc80f6a70fd91277b2ce9188
CoderPuppy <cpup@cpup-laptop>
2014-07-08 13:25:00 -0400

    Fix build-dev.sh

--- build-dev.sh
+++ build-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # watchify            client.js -d -o public/index.js &
-watchify client.js pages/vote/client.js -d -t brfs -o public/vote.js  &
+watchify -t brfs -d -o public/vote.js client.js pages/vote/client.js &
 stylus -w pages/**/*.styl public/**/*.styl &
