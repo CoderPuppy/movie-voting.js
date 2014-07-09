171eb6384f9d29c6708e27313dbaa691c9d9e380
CoderPuppy <cpup@cpup-laptop>
2014-07-09 09:57:44 -0400

    Use process.env.PORT and process.env.IP

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ const server = http.createServer(function(req, res) {
 		})
 	}
 })
-listen(server, 3000, '0.0.0.0')
+listen(server, process.env.PORT || 3000, process.env.IP || '0.0.0.0')
 
 const shoe = require('shoe')(require('client-reloader')(function(conn) {
 	conn.pipe(MuxDemux(function(stream) {
