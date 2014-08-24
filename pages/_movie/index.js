a86b3efd5feca4fac036358c943b6a3b9edee317
CoderPuppy <cpup@cpup-laptop>
2014-08-24 09:36:02 -0400

    Make _movie include the id

--- pages/_movie/index.js
+++ pages/_movie/index.js
@@ -1,12 +1,13 @@
 const hyperglue = require('hyperglue')
-const html      = require('fs').readFileSync(__dirname + '/index.html')
+const html      = require('fs').readFileSync(__dirname + '/index.html').toString()
 
 module.exports = function(movie) {
 	return hyperglue(html, {
 		'.name': movie.name,
 		'.desc': movie.desc,
 		'.movie': {
-			'data-id': movie.id.toString()
+			'data-id': movie.id.toString(),
+			'data-name': movie.name
 		}
 	})
 }
