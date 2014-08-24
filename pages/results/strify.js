3dcc13b70b040028abef34aab008339aa6c81e7f
CoderPuppy <cpup@cpup-laptop>
2014-08-24 09:57:46 -0400

    Switch noone to nobody in results Lilia doesn't like the proper spelling of noone (neither do I)

--- pages/results/strify.js
+++ pages/results/strify.js
@@ -2,7 +2,7 @@ module.exports = function(results) {
 	if(results.length == 1) {
 		return results[0] + ' won!'
 	} else if(results.length == 0) {
-		return 'Uh, noone voted...'
+		return 'Uh, nobody voted...'
 	} else {
 		return results.slice(0, -1).join(', ') + ' and ' + results.slice(-1)[0] + ' tied'
 	}
