640a41362fe109a23a307fb7cf76153e9f333df7
CoderPuppy <cpup@cpup-laptop>
2014-08-31 22:37:04 -0400

    Just changing stuff with condorcets

--- voting/condorcets.js
+++ voting/condorcets.js
@@ -13,14 +13,78 @@ function baseMatrix(movies) {
 module.exports = function(data) {
 	const voting = new EE
 
+	var titles
+
+	function updateTitles() {
+		titles = data.movies.map(function(movie) {
+			return movie.name.replace(/a|an|and|the|\s+/gi, '').replace(/[^A-Z:]/g, '')
+		})
+
+		titles.maxLength = Math.max.apply(Math, [0].concat(titles.map(function(title) {
+			return title.length
+		})))
+	}
+	updateTitles()
+
+	function renderMatrix(matrix, numbers) {
+		if(titles.length == 0)
+			return ''
+
+		var res = new Array(titles.maxLength + 2).join(' ') + titles.join(' ')
+		for(var y = 0; y < matrix.shape[1]; y++) {
+			res += '\n' + titles[y] + new Array(titles.maxLength - titles[y].length + 2).join(' ')
+			for(var x = 0; x < matrix.shape[0]; x++) {
+				var num = matrix.get(x, y)
+				var content = num ? '<' : ' '
+				if(numbers && num)
+					content = num + ''
+				res += content + new Array(Math.max(titles[x].length + 1 - content.length, 0)).join(' ') + ' '
+			}
+			res = res.slice(0, res.length - 1)
+		}
+		return res
+	}
+
+	function generateMatrix(person) {
+		const vote = person.vote
+
+		const moviesOrder = {}
+		data.movies.forEach(function(movie, id) {
+			moviesOrder[id] = Infinity
+		})
+		vote.forEach(function(id, i) {
+			moviesOrder[id] = i
+		})
+		debug('movies order:', moviesOrder)
+
+		const matrix = baseMatrix(data.movies)
+		var val
+		for(var x = 0; x < matrix.shape[0]; x++) {
+			for(var y = 0; y < matrix.shape[1]; y++) {
+				if(moviesOrder[y] < moviesOrder[x]) {
+					val = 1
+				} else {
+					val = 0
+				}
+				matrix.set(x, y, val)
+				debug.trace('setting %d (%d), %d (%d) to %d', x, moviesOrder[x], y, moviesOrder[y], val)
+			}
+		}
+		debug('generated matrix for %s (%d):', person.name, person.id)
+		if(debug.enabled) console.log(renderMatrix(matrix))
+		return matrix
+	}
+
 	function update() {
-		voting.matrix = voting.people.reduce(function(acc, val) {
-			ndarray.ops.add(acc, acc, val)
-			return acc
-		}, baseMatrix(data.movies))
+		updateTitles()
+
+		voting.matrix = baseMatrix(data.movies)
+		voting.people.forEach(function(matrix) {
+			console.log(ndarray.ops.add(voting.matrix, voting.matrix, matrix))
+		})
 
 		debug('matrix:')
-		if(debug.enabled) console.log(ndarray.show(voting.matrix))
+		if(debug.enabled) console.log(renderMatrix(voting.matrix, true))
 
 		voting.results = []
 	
@@ -48,10 +112,14 @@ module.exports = function(data) {
 		(function() {
 			var maxWins = 0
 			for(var id = 0; id < data.movies.length; id++) {
-				if(wins[id] >= data.movies.length) {
+				/*if(wins[id] >= data.movies.length - 1) {
 					voting.results = [ id ]
 					return
-				} else if(wins[id] >= maxWins) {
+				} else */
+				if(wins[id] >= maxWins) {
+					debug('adding %s (%d) - %d to the tie %s at %d', data.movies[id].name, id, wins[id], voting.results.map(function(id) {
+						return data.movies[id].name
+					}).join(', '), maxWins)
 					if(wins[id] > maxWins) {
 						maxWins = wins[id]
 						voting.results = []
@@ -67,38 +135,10 @@ module.exports = function(data) {
 		if(voting.results.length == data.movies.length)
 			voting.results = []
 
-		const titles = data.movies.map(function(movie) {
-			return movie.name.replace(/a|an|and|the|\s+/gi, '').replace(/[^A-Z:]/g, '')
-		})
-
-		titles.maxLength = Math.max.apply(Math, [0].concat(titles.map(function(title) {
-			return title.length
-		})))
-		console.log(titles.maxLength)
-
-		function renderMatrix(matrix, numbers) {
-			var res = new Array(titles.maxLength + 2).join(' ') + titles.join(' ')
-			for(var y = 0; y < matrix.shape[1]; y++) {
-				res += '\n' + titles[y] + new Array(titles.maxLength - titles[y].length + 2).join(' ')
-				for(var x = 0; x < matrix.shape[0]; x++) {
-					var num = matrix.get(x, y)
-					var content = num ? '<' : ' '
-					if(numbers && num)
-						content = num + ''
-					// console.log(titles[x], content.length)
-					res += content + new Array(Math.max(titles[x].length + 1 - content.length, 0)).join(' ') + ' '
-				}
-				res = res.slice(0, res.length - 1)
-			}
-			return res
-		}
-
-		// console.log(titles)
 		voting.debugInfo = 'Result:\n' + renderMatrix(voting.matrix, true).split('\n').map(function(line) {
 			return '  ' + line
 		}).join('\n') + '\n\n' + voting.people.map(function(matrix, i) {
 			const person = data.people[i]
-			console.log(person.vote)
 			return person.name + ': ' + person.vote.map(function(i) {
 				return titles[i]
 			}).join(' ') + '\n' + renderMatrix(matrix).split('\n').map(function(line) {
@@ -116,7 +156,7 @@ module.exports = function(data) {
 	voting.update = function(person) { return function(cb) {
 		if(!cb) cb = function(err) { if(err) throw err }
 
-		voting.people[person.id] = generateMatrix(person, data.movies)
+		voting.people[person.id] = generateMatrix(person)
 		update()
 
 		process.nextTick(function() { cb(null) })
@@ -144,34 +184,4 @@ module.exports = function(data) {
 	} }
 
 	return voting
-}
-
-function generateMatrix(person, movies) {
-	const vote = person.vote
-
-	const moviesOrder = {}
-	movies.forEach(function(movie, id) {
-		moviesOrder[id] = Infinity
-	})
-	vote.forEach(function(id, i) {
-		moviesOrder[id] = i
-	})
-	debug('movies order:', moviesOrder)
-
-	const matrix = baseMatrix(movies)
-	var val
-	for(var x = 0; x < matrix.shape[0]; x++) {
-		for(var y = 0; y < matrix.shape[1]; y++) {
-			if(moviesOrder[y] < moviesOrder[x]) {
-				val = 1
-			} else {
-				val = 0
-			}
-			matrix.set(x, y, val)
-			debug.trace('setting %d (%d), %d (%d) to %d', x, moviesOrder[x], y, moviesOrder[y], val)
-		}
-	}
-	debug('generated matrix for %s (%d):', person.name, person.id)
-	if(debug.enabled) console.log(ndarray.show(matrix))
-	return matrix
 }
